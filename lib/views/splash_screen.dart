edc1e397d5eae12dbaa3a506fd53f9e5885229f0
Muhammad Usama <imusamairshad@gmail.com>
2023-10-20 23:22:17 +0500

    splash_screen.dart is modified.

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,16 +1,53 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 3),
+  )..repeat();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [],
+          children: [
+            AnimatedBuilder(
+                animation: _animationController,
+                child: SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: Center(
+                    child: Image.asset("assets/images/logo.png"),
+                  ),
+                ),
+                builder: (context, child) {
+                  return Transform.rotate(
+                    angle: _animationController.value * 2.0 * math.pi,
+                    child: child,
+                  );
+                }),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .08,
+            ),
+          ],
         ),
       ),
     );
